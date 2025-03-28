863aeb302dcbd803e08b7dc5933b835cd1c4dc5e
Alexandra <a.yuhno@tg.dunice.net>
2025-03-28 16:22:34 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <SlotList>
+    <template #header>
+      <h2>Cписок</h2>
+    </template>
+
+    <template #default>
+      <span>Элемент 1</span>
+      <span>Элемент 2</span>
+      <span>Элемент 3</span>
+    </template>
+
+    <template #footer>
+      <p>© 2025</p>
+    </template>
+  </SlotList>
+</template>
+
+<script setup>
+import SlotList from "./components/SlotList.vue";
+</script>
