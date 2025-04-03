eafc297c3cded8600ea1fbc3a89abeae13d1ef39
Alexandra <a.yuhno@tg.dunice.net>
2025-04-03 11:18:49 +0300

    Update convertStyle.ts

--- convertStyle.ts
+++ convertStyle.ts
@@ -2,7 +2,6 @@ type StyleObject = { backgroundColor?: string };
 
 function convertStyle(style: StyleObject): void;
 function convertStyle(key: string, style: StyleObject): void;
-function convertStyle(key: string): void;
 
 function convertStyle(arg1: string | StyleObject, arg2?: StyleObject): void {
     /*логика*/
