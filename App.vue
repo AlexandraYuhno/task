d9aa1f73fd973758d632b9c20fd35944200d4013
Alexandra <a.yuhno@tg.dunice.net>
2025-04-03 11:17:41 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,13 +1,13 @@
 <template>
-  <SlotList>
+  <SlotList v-model="items">
     <template #header>
-      <h2>Cписок</h2>
+      <h2>Список</h2>
     </template>
 
     <template #default>
-      <span>Элемент 1</span>
-      <span>Элемент 2</span>
-      <span>Элемент 3</span>
+      <template v-for="(item, index) in items" :key="index">
+        <span>{{ item }}</span>
+      </template>
     </template>
 
     <template #footer>
@@ -17,5 +17,8 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import SlotList from "./components/SlotList.vue";
+
+const items = ref(["Элемент 1", "Элемент 2", "Элемент 3"]);
 </script>
